247d0be4395ced6966fd046dd14db845ccbd82c0
yoneda <daisuke.yoneda@basic.co.jp>
2013-06-28 20:30:29 +0900

    新規作成

---
+++ LinqStudyCS/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+//とりあえず最初はLinqなしで
+//using System.Linq; 
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqStudyCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
