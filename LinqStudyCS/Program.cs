f7ccea201c7609a54090168a0fe23be9b95ce617
yoneda <daisuke.yoneda@basic.co.jp>
2013-06-28 21:44:58 +0900

    とりあえず、べた書き。

--- LinqStudyCS/Program.cs
+++ LinqStudyCS/Program.cs
@@ -11,6 +11,58 @@ namespace LinqStudyCS
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("(C#)Problem1:" + Problem1(1000));
+            Console.WriteLine("(C#)Problem2:" + Problem2(4000000));
+        }
+
+        // http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%201
+        /// <summary>
+        /// 10未満の自然数のうち, 3 もしくは 5 の倍数になっているものは 3, 5, 6, 9 の4つがあり, これらの合計は 23 になる.
+        /// 同じようにして, 1000 未満の 3 か 5 の倍数になっている数字の合計を求めよ.
+        /// </summary>
+        /// <param name="limit">制限値</param>
+        /// <returns>答え</returns>
+        public static int Problem1(int limit)
+        {
+            int sum = 0;
+            for (int i = 0; i < limit; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+
+        // http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%202
+        /// <summary>
+        /// フィボナッチ数列の項は前の2つの項の和である. 最初の2項を 1, 2 とすれば, 最初の10項は以下の通りである.
+        /// 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+        /// 数列の項の値が400万を超えない範囲で, 偶数値の項の総和を求めよ.
+        /// </summary>
+        /// <param name="limit">制限値</param>
+        /// <returns>答え</returns>
+        public static int Problem2(int limit)
+        {
+            int sum = 0;
+
+            int a = 1;
+            int b = 1;
+            while (b < limit)
+            {
+                if (b % 2 == 0)
+                {
+                    sum += b;
+                }
+
+                var temp = a;
+                a = b;
+                b = a + temp;
+            }
+
+            return sum;
         }
     }
 }
--- LinqStudyVB/Module1.vb
+++ LinqStudyVB/Module1.vb
@@ -1,7 +1,50 @@
 ﻿Module Module1
 
     Sub Main()
-
+        Console.WriteLine("(VB)Problem1:" + CStr(Problem1(1000)))
+        Console.WriteLine("(VB)Problem2:" + CStr(Problem2(4000000)))
     End Sub
+
+    '' http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%201
+    ''' <summary>
+    ''' 10未満の自然数のうち, 3 もしくは 5 の倍数になっているものは 3, 5, 6, 9 の4つがあり, これらの合計は 23 になる.
+    ''' 同じようにして, 1000 未満の 3 か 5 の倍数になっている数字の合計を求めよ.
+    ''' </summary>
+    ''' <param name="limit">制限値</param>
+    ''' <returns>答え</returns>
+    ''' <remarks></remarks>
+    Public Function Problem1(ByVal limit As Integer) As Integer
+        Dim sum As Integer = 0
+        For i = 0 To limit - 1
+            If i Mod 3 = 0 OrElse i Mod 5 = 0 Then
+                sum += i
+            End If
+        Next
+        Return sum
+    End Function
+
+    '' http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%202
+    ''' <summary>
+    ''' フィボナッチ数列の項は前の2つの項の和である. 最初の2項を 1, 2 とすれば, 最初の10項は以下の通りである.
+    ''' 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+    ''' 数列の項の値が400万を超えない範囲で, 偶数値の項の総和を求めよ.
+    ''' </summary>
+    ''' <param name="limit">制限値</param>
+    ''' <returns>答え</returns>
+    ''' <remarks></remarks>
+    Public Function Problem2(ByVal limit As Integer)
+        Dim sum = 0
+        Dim a = 1
+        Dim b = 1
+        While b < limit
+            If b Mod 2 = 0 Then
+                sum += b
+            End If
+            Dim temp = a
+            a = b
+            b = a + temp
+        End While
+        Return sum
+    End Function
 
 End Module
